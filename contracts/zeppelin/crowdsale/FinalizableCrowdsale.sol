aca6df54c7d5f509166dbb41b0730d4105622fe2
Igor Sokolov <igorgrru@gmail.com>
2017-09-11 19:01:31 +0700

    Updated OpenZeppelin smart contracts. Added MultiSigWallet smart contract from Gnosis.

--- contracts/zeppelin/crowdsale/FinalizableCrowdsale.sol
+++ contracts/zeppelin/crowdsale/FinalizableCrowdsale.sol
@@ -7,7 +7,7 @@ import './Crowdsale.sol';
 /**
  * @title FinalizableCrowdsale
  * @dev Extension of Crowsdale where an owner can do extra work
- * after finishing. By default, it will end token minting.
+ * after finishing. 
  */
 contract FinalizableCrowdsale is Crowdsale, Ownable {
   using SafeMath for uint256;
@@ -16,8 +16,10 @@ contract FinalizableCrowdsale is Crowdsale, Ownable {
 
   event Finalized();
 
-  // should be called after crowdsale ends, to do
-  // some extra finalization work
+  /**
+   * @dev Must be called after crowdsale ends, to do some extra finalization
+   * work. Calls the contract's finalization function.
+   */
   function finalize() onlyOwner {
     require(!isFinalized);
     require(hasEnded());
@@ -28,12 +30,11 @@ contract FinalizableCrowdsale is Crowdsale, Ownable {
     isFinalized = true;
   }
 
-  // end token minting on finalization
-  // override this with custom logic if needed
+  /**
+   * @dev Can be overriden to add finalization logic. The overriding function
+   * should call super.finalization() to ensure the chain of finalization is
+   * executed entirely.
+   */
   function finalization() internal {
-    token.finishMinting();
   }
-
-
-
 }
